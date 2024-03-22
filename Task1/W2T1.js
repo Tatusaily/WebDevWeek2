697b1f6e9cf481908a925936fc217666ef07d3e9
Tatu <tatu.sailynoja@gmail.com>
2024-03-22 19:37:32 +0200

    Task1 Done

--- Task1/W2T1.js
+++ Task1/W2T1.js
@@ -1,7 +1,15 @@
 //Celcius to Fahrenheit and Kelvin
-function celsiusToFahrenheitAndKelvin(celsius) {
+function celsiusToFahrenheitAndKelvin(target) {
+    // Input
+    const celsius = parseFloat(prompt("Enter the temperature in Celsius: "));
     let fahrenheit = celsius * 9 / 5 + 32;
     let kelvin = celsius + 273.15;
-    return [fahrenheit, kelvin];
+    
+    // Output
+    target.appendChild(document.createTextNode(`Temperature in Fahrenheit: ${fahrenheit}°F`));
+    target.appendChild(document.createElement("br"));
+    target.appendChild(document.createTextNode(`Temperature in Kelvin: ${kelvin}K`));
 }
 
+// Input
+celsiusToFahrenheitAndKelvin(document.querySelector("#outputDiv"));
