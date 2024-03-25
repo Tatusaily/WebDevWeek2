a51ee9bcdae80443a642871e23ad7fe28593f71a
Tatusaily <tatu.sailynoja@gmail.com>
2024-03-25 15:36:41 +0200

    completed t1

--- Recap_3/t1/t1.js
+++ Recap_3/t1/t1.js
@@ -2,29 +2,39 @@
 const todoList = [
   {
     id: 1,
-    task: 'Learn HTML',
+    task: "Learn HTML",
     completed: true,
   },
   {
     id: 2,
-    task: 'Learn CSS',
+    task: "Learn CSS",
     completed: true,
   },
   {
     id: 3,
-    task: 'Learn JS',
+    task: "Learn JS",
     completed: false,
   },
   {
     id: 4,
-    task: 'Learn TypeScript',
+    task: "Learn TypeScript",
     completed: false,
   },
   {
     id: 5,
-    task: 'Learn React',
+    task: "Learn React",
     completed: false,
   },
 ];
 
 // add your code here
+const target = document.querySelector("ul");
+todoList.forEach((item) => {
+  const html = `
+    <li>
+      <input type="checkbox" id="todo-${item.id}" ${item.completed ? "checked" : ""}>
+      <label for="todo-${item.id}">${item.task}</label>
+    </li>
+    `;
+  target.insertAdjacentHTML("beforeend", html);
+});
