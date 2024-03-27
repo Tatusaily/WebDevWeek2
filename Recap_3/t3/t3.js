a3780ffced150c9c445b33f3b0082e5b01315f37
Tatusaily <tatu.sailynoja@gmail.com>
2024-03-27 14:04:03 +0200

    teht3 final

--- Recap_3/t3/t3.js
+++ Recap_3/t3/t3.js
@@ -1,30 +1,29 @@
-
 // When the page is loaded, we call the function displayInfo
-function displayInfo(){
-    // First we get info and then we put them in the div
-    // Windows dimensions
-    let wWidth = window.innerWidth;
-    let wHeight = window.innerHeight;
+function displayInfo() {
+  // First we get info and then we put them in the div
+  // Windows dimensions
+  const wWidth = window.innerWidth;
+  const wHeight = window.innerHeight;
 
-    // Screen dimensions
-    let sWidth = screen.width;
-    let sHeight = screen.height;
-    let saWidth = screen.availWidth;
-    let saHeight = screen.availHeight;
+  // Screen dimensions
+  const sWidth = screen.width;
+  const sHeight = screen.height;
+  const saWidth = screen.availWidth;
+  const saHeight = screen.availHeight;
 
-    // Browser name and version
-    let browserinfo = navigator.userAgent;
+  // Browser name and version
+  const browserinfo = navigator.userAgent;
 
-    // Current time
-    let time = new Date();
-    let hours = time.getHours();
-    let minutes = time.getMinutes();
-    let seconds = time.getSeconds();
+  // Current time
+  const time = new Date();
+  const hours = time.getHours();
+  const minutes = time.getMinutes();
+  const seconds = time.getSeconds();
+  const date = `${time.getDate()}.${time.getMonth() + 1}.${time.getFullYear()}`;
 
-
-    // Put the info in the div
-    const target = document.querySelector('#target');
-    target.innerHTML = `
+  // Put the info in the div
+  const target = document.querySelector('#target');
+  target.innerHTML = `
         <p>Window width: ${wWidth}</p>
         <p>Window height: ${wHeight}</p>
         <p>Screen width: ${sWidth}</p>
@@ -32,10 +31,11 @@ function displayInfo(){
         <p>Screen available width: ${saWidth}</p>
         <p>Screen available height: ${saHeight}</p>
         <p>Browser info: ${browserinfo}</p>
+        <p>Current date: ${date}</p>
         <p>Current time: ${hours}:${minutes}:${seconds}</p>
     `;
-    // Displayinfo calls itself again after 1 second
-    setTimeout(displayInfo, 1000);
+  // Displayinfo calls itself again after 1 second
+  setTimeout(displayInfo, 1000);
 }
 
 // Call the function displayInfo
