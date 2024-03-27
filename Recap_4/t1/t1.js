97473e35ed0a658a6f1383db1dc705d86c0c2716
Tatu <tatu.sailynoja@gmail.com>
2024-03-27 18:40:14 +0200

    task1 done

--- Recap_4/t1/t1.js
+++ Recap_4/t1/t1.js
@@ -28,3 +28,62 @@ const todoList = [
 ];
 
 // add your code here
+function liAdder(item, target) {
+  const li = document.createElement('li');
+
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  checkbox.id = `todo- ${item.id}`;
+  checkbox.checked = item.completed;
+  checkbox.addEventListener('change', () => {
+    todoList.find((todo) => todo.id === item.id).completed = checkbox.checked;
+    console.log(todoList);});
+  li.appendChild(checkbox);
+
+  const label = document.createElement('label');
+  label.htmlFor = `todo- ${item.id}`;
+  label.textContent = item.task;
+  li.appendChild(label);
+
+  const deletebutton = document.createElement('button');
+  deletebutton.textContent = 'Delete';
+  deletebutton.addEventListener('click', () => {
+    todoList.splice(todoList.findIndex((todo) => todo.id === item.id), 1);
+    target.removeChild(li);
+    console.log(todoList);
+  });
+  li.appendChild(deletebutton);
+
+  target.appendChild(li);
+
+}
+
+// Define target ul
+const target = document.querySelector('ul');
+// Add all from todolist
+todoList.forEach((item) => {
+  liAdder(item, target);
+});
+
+// Adder button & popup button functionality
+document.querySelector('.add-btn').addEventListener('click', () => {
+  document.querySelector('dialog').showModal();
+});
+
+document.querySelector('form button').addEventListener('click', (event) => {
+  event.preventDefault();
+  const text = document.querySelector('form input').value;
+  if (text === '') return;
+  const id = todoList.length + 1;
+
+  const task = {
+    id: id,
+    task: text,
+    completed: false,
+  }
+
+  todoList.push(task);
+  liAdder(task, target);
+
+  document.querySelector('dialog').close();
+});
